91b38a0f50bbdef83f69a8b8b39701eebfddc4b2
jcl <jorgecano86@gmail.com>
2019-09-10 18:15:16 +0200

    Animaciones con js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    $('main-button').click(function() {
-        alert('ei');
+
+    var milliseconds = 500;
+
+    $('#main-button').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#portfolio-section').offset().top
+        }, milliseconds);
+    });
+
+    $('#portfolio-button').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#who-am-i-section').offset().top
+        }, milliseconds);
+    });
+
+    $('#who-am-i-button').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#contact-section').offset().top
+        }, milliseconds);
+    });
+
+    $('#go-up-button').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#hero-section').offset().top
+        }, milliseconds);
     });
 });
